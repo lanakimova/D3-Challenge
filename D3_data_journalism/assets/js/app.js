7eb80d5aec77717995f05f46503de2d22c7da551
Svetlana Akimova <lana@Svetlanas-MacBook-Pro.local>
2021-02-11 16:13:12 -0800

    add text to scatter plot, do refactoring

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,16 +1,53 @@
 // @TODO: YOUR CODE HERE!
 
-d3.csv("assets/data/data.csv").then(function(data) {
+// set size for svg container
+let height = 600;
+let width = 1000;
 
-    // get data from csv file
-    let states = [];
-    let abbrs = [];
-    let poverties = [];
-    let ages = [];
-    let incomes = [];
-    let healthCare = [];
-    let obesity = [];
-    let smokes = [];
+// set margins for svg container
+let margin = {
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50
+};
+
+// set size of chart area minus margins
+let chartHeight = height - margin.top - margin.bottom;
+let chartWidth = width - margin.left - margin.right;
+
+// create svg container
+let svg = d3.select("#scatter")
+.append("svg")
+.attr("height", height)
+.attr("width", width);
+
+// shift all over by the margin
+let chartGroup = svg.append("g").attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// add title for xaxis
+svg.append("text")
+    .attr("x", width/2)
+    .attr("y", height - 15)
+    .text("In Poverty (%)");
+
+//  add title for yaxis
+svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -(height/2 + 50))
+    .attr("y", 20)
+    .text("Lacks of Healthcare (%)");
+
+
+d3.csv("assets/data/data.csv").then(function(data) {
+    let states = [],
+        abbrs = [],
+        poverties = [],
+        ages = [],
+        incomes = [],
+        healthCare = [],
+        obesity = [],
+        smokes = [];
 
     data.forEach((d) => {
         states.push(d.state);
@@ -22,43 +59,11 @@ d3.csv("assets/data/data.csv").then(function(data) {
         obesity.push(+d.obesity);
         smokes.push(+d.smokes);
     });
-    
-    // set size for svg container
-    let height = 600;
-    let width = 1000;
-
-    // set margins for svg container
-    let margin = {
-        top: 50,
-        right: 50,
-        bottom: 50,
-        left: 50
-    };
-
-    // set size of chart area minus margins
-    let chartHeight = height - margin.top - margin.bottom;
-    let chartWidth = width - margin.left - margin.right;
-
-
-    // create svg container
-    let svg = d3.select("#scatter")
-                .append("svg")
-                .attr("height", height)
-                .attr("width", width);
-
-    // shift all over by the margin
-    let chartGroup = svg.append("g").attr("transform", `translate(${margin.left}, ${margin.top})`);
 
     // scale y 
     let yScale = d3.scaleLinear()
                     .domain([d3.min(healthCare), d3.max(healthCare)])
                     .range([chartHeight, 0]); 
-
-    console.log(`Healthcare: ${healthCare}`);   
-    console.log(`MIN: ${d3.min(healthCare)}, MAX: ${d3.max(healthCare)}`);
-
-    console.log(`poverty ${poverties}`);
-    console.log(`MIN: ${d3.min(poverties)}, MAX: ${d3.max(poverties)}`);
 
     // scale x
     let xScale = d3.scaleLinear()
@@ -76,40 +81,36 @@ d3.csv("assets/data/data.csv").then(function(data) {
     .attr("transform", `translate(0, ${chartHeight})`)
     .call(xAxis);
 
-
-
     // create the rectangle
-
-    let barsGroup = chartGroup.selectAll("rect")
+    let circleGroup = chartGroup.selectAll("rect")
                                 .data(healthCare)
                                 .enter()
                                 .append('circle')
                                 .attr("cx", function(d, i) {return xScale(poverties[i]);})
                                 .attr("cy", function(d, i) {return yScale(healthCare[i]);})
-                                .attr("r", 2.5)
-                                .style('fill', 'blue');
-
-    svg.append("text")
-        // .attr("text-anchor", "end")
-        .attr("x", width/2)
-        .attr("y", height - 15)
-        .text("In Poverty (%)");
-
-    svg.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("x", -(height/2 + 50))
-        .attr("y", 20)
-        .text("Lacks of Healthcare (%)");
+                                .attr("opacity", 0.5)
+                                .attr("r", 10)
+                                .attr("stroke", "black")
+                                .style('fill', '#bde0fe');
                                 
-    barsGroup.on("mouseover", function() {
+                                
+    //add abbr of states to each circle   
+    chartGroup.selectAll("rect")
+                .data(abbrs).enter()
+                .append("text")
+                .attr("x", function(d, i) {return xScale(poverties[i])-5;})
+                .attr("y", function(d, i) {return yScale(healthCare[i]) +2;})
+                .attr("font-size", "8px")
+                .attr("font-weight", "bold")
+                .text(function(d, i) {return abbrs[i]});
+                            
+    circleGroup.on("mouseover", function() {
         d3.select(this)
             .transition()
             .duration(1500)
-            .style('fill', 'red');
+            .style('fill', '#606c38');
     }).on("mouseout", function() {
-        d3.select(this).transition().duration(1500).style("fill", "blue");
+        d3.select(this).transition().duration(1500).style("fill", "#ffc8dd");
     });
-
-                                
 
 });
